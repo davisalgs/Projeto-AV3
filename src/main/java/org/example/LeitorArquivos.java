7eb39f402c6f74013f6a4b88966522522ca2b798
davisalgs <salgueirodavi01@gmail.com>
2024-12-04 10:21:37 -0300

    Falta apenas o output

--- src/main/java/org/example/LeitorArquivos.java
+++ src/main/java/org/example/LeitorArquivos.java
@@ -8,13 +8,14 @@ import java.util.List;
 
 public class LeitorArquivos {
     public static String lerTexto() throws IOException {
-        List<String> linhas = Files.readAllLines(Paths.get("D:\\Projetos\\Java\\Projeto-AV3-ED\\src\\main\\java\\org\\example\\texto.txt"), StandardCharsets.UTF_8);
+        List<String> linhas = Files.readAllLines(Paths.get("src","main","java","org","example","texto.txt"), StandardCharsets.UTF_8);
 
         return String.join("\n", linhas);
     }
 
     public static String lerArgumentos() throws IOException {
-        List<String> linhas = Files.readAllLines(Paths.get("D:\\Projetos\\Java\\Projeto-AV3-ED\\src\\main\\java\\org\\example\\palavras-chave.txt"), StandardCharsets.UTF_8);
+        List<String> linhas = Files.readAllLines(Paths.get("src","main","java","org","example","palavras-chave.txt"), StandardCharsets.UTF_8);
+        //List<String> linhas = Files.readAllLines(Paths.get("D:\\Projetos\\Java\\Projeto-AV3-ED\\src\\main\\java\\org\\example\\palavras-chave.txt"), StandardCharsets.UTF_8);
 
 
         return String.join("\n", linhas);
